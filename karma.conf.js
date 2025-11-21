a07a2f8bcb78cdd3344ef3f352c04b68f1f152aa
Oscar Sinuco <oscarsinuco@gmail.com>
2025-11-21 14:31:53 -0500

    ci: add production config to build and update artifact paths for Angular 18+

--- karma.conf.js
+++ karma.conf.js
@@ -24,11 +24,12 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bad-practices-project'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcov' }
       ]
     },
     // Add JUnit reporter configuration
